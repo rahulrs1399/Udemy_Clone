25b0a3527a8d0b64e35359a131fdc0568ef9b1bc
Rahul_RS <venom910405@gmail.com>
2024-12-30 20:57:44 +0530

    save point

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,23 +1,41 @@
 import { Link } from "react-router-dom";
 import { GraduationCap } from "lucide-react";
-import { Tabs } from "@radix-ui/react-tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useState } from "react";
-
+import CommonForm from "@/components/common-form";
+import { signUpFormControls } from "@/config";
 
 function AuthPage() {
+  const [activeTab, setActiveTab] = useState("signin");
 
-    const [activeTab, setActiveTab] = useState('signin');
-    
+  function handleTabChange(value) {
+    setActiveTab(value);
+  }
+ 
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center border-b">
-      <Link to={"/"} className="flex items-center justify-center">
+        <Link to={"/"} className="flex items-center justify-center">
           <GraduationCap className="h-8 w-8 mr-4" />
           <span className="font-extrabold text-xl">LMS LEARN</span>
         </Link>
       </header>
       <div className="flex items-center justify-center min-h-screen bg-background">
-        <Tabs></Tabs>
+        <Tabs
+          value={activeTab}
+          defaultValue="signin"
+          onValueChange={handleTabChange}
+          className="w-full max-w-md"
+        >
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="signin">Sign In</TabsTrigger>
+            <TabsTrigger value="signup">Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signin">signin</TabsContent>
+          <TabsContent value="signup">
+            <CommonForm formControl={signUpFormControls} />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
